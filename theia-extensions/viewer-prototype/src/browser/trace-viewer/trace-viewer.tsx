235313bea068b7ba08ee753c7129297cc17bba45
Will Yang <william.yang@ericsson.com>
2022-03-14 18:45:48 -0500

    fixing bernd's comments

--- theia-extensions/viewer-prototype/src/browser/trace-viewer/trace-viewer.tsx
+++ theia-extensions/viewer-prototype/src/browser/trace-viewer/trace-viewer.tsx
@@ -1,5 +1,5 @@
 import { DisposableCollection, MessageService, Path } from '@theia/core';
-import { ApplicationShell, Message, StatusBar, WidgetManager } from '@theia/core/lib/browser';
+import { ApplicationShell, Message, StatusBar, WidgetManager, StatefulWidget } from '@theia/core/lib/browser';
 import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
 import { inject, injectable, postConstruct } from 'inversify';
 import * as React from 'react';
@@ -28,7 +28,7 @@ export interface TraceViewerWidgetOptions {
 }
 
 @injectable()
-export class TraceViewerWidget extends ReactWidget {
+export class TraceViewerWidget extends ReactWidget implements StatefulWidget {
     static ID = 'trace-viewer';
     static LABEL = 'Trace Viewer';
 
@@ -245,7 +245,7 @@ export class TraceViewerWidget extends ReactWidget {
         return this.outputDescriptors;
     }
 
-    restoreState(state: OutputDescriptor[]) {
+    restoreState(state: OutputDescriptor[]): void {
         this.outputDescriptors = state;
     }
 
